ff233388cf368ebc03624822378c6d8aadd2c092
VRamazing <r.vignesh149@gmail.com>
2017-09-27 20:06:42 +0530

    Line Ball Complete

--- js/index.js
+++ js/index.js
@@ -2,13 +2,22 @@ var canvas;
 var ctx;
 var score = 0;
 
-const BALL_X = 70;
+const BALL_X = 50;
 var ball_Y ;
-var ballSize = 15;
+const BALL_SIZE = 15;
 
-const GRAVITY = 8 ;
-const DRAG = -8;
+const GRAVITY = 9 ;
+const DRAG = -9;
 var ballYV = GRAVITY; //Initially let gravity act
+
+const PIPE_WIDTH = 15;
+var pipeXV = 9 ;
+// var pipe_X ;
+
+var pipes = [];
+var score = 0;
+
+var gameOver; //boolean
 
 window.onload = () => {
   canvas = document.getElementById("mainGame");
@@ -16,104 +25,171 @@ window.onload = () => {
   ctx.fillStyle = "white";
   ctx.fillRect(0,0,canvas.width,canvas.height);
   ball_Y = canvas.height/2;
+  gameOver = false;
 
-  document.addEventListener('keydown',handleKeyDown);
-  document.addEventListener('keyup',handleKeyUp);
+
+  document.addEventListener('keydown',function(evt){
+      if(evt.code == "Space"){
+
+          ballYV = DRAG;
+
+      }});
+
+  document.addEventListener('keyup',function(evt){
+      if(evt.code == "Space"){
+
+            ballYV = GRAVITY;
+
+        }
+  });
+
+  document.addEventListener('mousedown',function(evt){
+   
+      if(gameOver == true){
+
+            gameOver = false;
+            gameReset();
+
+        }
+  });
   
   var framesPerSecond = 60;
   setInterval(mainGame,1000/framesPerSecond);
- 
-};
+  var pipeGeneratePerSecond = 2;
+  setInterval(generatePipes,1000/pipeGeneratePerSecond);
+
+}; //initializing function
 
 
 var mainGame = () => {
- 
-  ball_Y += ballYV; 
 
-  ctx.fillStyle = "white";
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-  
-  ctx.fillStyle = "orange"
-  ctx.beginPath();
-  ctx.arc(BALL_X,ball_Y,ballSize,0,Math.PI*2);
-  ctx.fill();
+  if(!gameOver){
+      
+      if(outsideBoundaries()){
+        gameOver = true;
+      };
+
+      ball_Y += ballYV; 
+
+      ctx.fillStyle = "white";
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+      
+      ctx.fillStyle = "salmon"
+      ctx.beginPath();
+      ctx.arc(BALL_X,ball_Y,BALL_SIZE,0,Math.PI*2);
+      ctx.fill();
+
+      pipes.forEach(function(elem,index){
+
+        if(elem.pipe_X < 0){
+
+          //delete the pipe if it moves out of canvas;
+          score++;
+          pipes.splice(index,1)
+
+        }
+        elem.pipe_X -= pipeXV;
+     
+        if (isColliding(elem)){
+            
+              gameOver = true;
+        }
+        
+        ctx.fillStyle = "#acacac";
+        ctx.fillRect(elem.pipe_X , elem.pipe_Y, PIPE_WIDTH, elem.pipeHeight);
+        
+      
+
+      });
+
+      ctx.fillStyle = "black";
+      ctx.font="20px Arial";
+      ctx.fillText("Score : " + score ,480,30);
+
+  }
+
+  else{
+
+      ctx.fillStyle = "salmon";
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+      ctx.fillStyle = "black";
+      ctx.font="60px Arial";
+      ctx.fillText("GAME OVER",112,150);
+      ctx.font="30px Arial";
+      ctx.fillText("Final Score : " + score , canvas.width/2 - 100,200);
+      ctx.fillStyle = "#474747";
+      ctx.font="20px Arial";
+      ctx.fillText("Click to Play ", canvas.width/2  - 60,250);
 
 
- 
- 
+  }
 
-  // ctx.fillStyle = "black";
-  // ctx.font="20px Arial";
-  // ctx.fillText("Score : " + score ,290,30);
-  // console.log(score);
+} //main game
+
+
+var gameReset = () => {
+  ball_Y = canvas.height/2;
+  pipes = [];
+  score = 0;
 }
 
 
 
+var outsideBoundaries = () => {
+
+  return ball_Y < 0 || ball_Y > canvas.height;
+
+}
 
 
-// var collision = () => {
-//   var bodyBox={
-//     x : xv - BODY_SIZE/2,
-//     y : yv - BODY_SIZE/2,
-//     width : BODY_SIZE,
-//     height : BODY_SIZE
-
-//   }
-//   var foodBox={
-//     x : foodX - FOOD_SIZE/2,
-//     y : foodY - FOOD_SIZE/2,
-//     width : FOOD_SIZE,
-//     height : FOOD_SIZE
-
-//   }
-
-//   return testCollisionRect(bodyBox,foodBox);
-// }
+var generatePipes = () => {
 
 
-// var  testCollisionRect = (rect1,rect2) => {
-//   return rect1.x <= rect2.x + rect2.width
-//     && rect2.x <= rect1.x + rect1.width
-//     && rect1.y <= rect2.y + rect2.height
-//     && rect2.y <= rect1.y + rect1.height;
+  var pipe =  {
+    pipeHeight : 0,
+    pipe_X : canvas.width,
+    pipe_Y: canvas.height,
+    pipeOrigin : 0
 
-//  }
-
-
-
-// var checkBoundaries = () => {
-//   if(xv < 0 ){
-//       xv = canvas.width - BODY_SIZE
-//   }
-//   else if (xv > canvas.width){
-//     xv = BODY_SIZE
-//   }
-//   else if(yv < 0 ){
-//       yv = canvas.height - BODY_SIZE
-//   }
-//   else if (yv > canvas.height){
-//     yv = BODY_SIZE
-//   }
-
-// }
+  };
 
 
-var handleKeyDown = (evt) => {
+  pipe.pipeHeight = Math.floor(Math.random()*(canvas.height - BALL_SIZE * 3));
 
-      if(evt.code == "Space"){
-          ballYV = DRAG;
+  pipe.pipeOrigin = Math.floor(Math.random()*2);
+  //0 -> means top ; 1 -> means bottom
+  if(pipe.pipeOrigin == 0){
+    pipe.pipe_Y = 0;
+
+  }
+  else{
+      pipe.pipe_Y = canvas.height - pipe.pipeHeight 
+  }
+
+  pipes.push(pipe);
+
+}
+
+
+
+var isColliding  = (pipe) => {
+ 
+  if(pipe.pipe_X <= BALL_X + BALL_SIZE && pipe.pipe_X >= BALL_X - BALL_SIZE){
+      if(pipe.pipeOrigin == 0  && pipe.pipeHeight > ball_Y){
+        return true;
       }
-}
-
-var handleKeyUp = (evt) => {
-
-      if(evt.code == "Space"){
-
-          ballYV = GRAVITY;
-
+      else if(pipe.pipeOrigin == 1  && pipe.pipe_Y < ball_Y){
+        return true;
       }
-    
+      else{
+        return false;
+      }
+
+  }
 
 
 }
+
+
+
+
